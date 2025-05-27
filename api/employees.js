c0636eb9713678262536d87f869d0d546dddea2b
Lyla Lynn <lyladlynn@gmail.com>
2025-05-27 15:21:46 -0500

    got routes working!

--- api/employees.js
+++ api/employees.js
@@ -1,27 +1,23 @@
 import { createEmployee, getEmployees } from "#db/queries/employees";
 import express from "express";
 const router = express.Router();
-export default router;
-
-router.route('/').get(async ( req, res ) => {
-    res.send('Welcome to the Fullstack Employees API.')
-})
-
-
-router.route("/").get(async ( req,res ) => {
-    const employees = await getEmployees();
-    res.send(employees);
-});
-
-router.route("/").post(async ( req, res ) => {
-    if(!req.body){
-        return res.status(400).send({error: 'Missing req.body'})
-    }
-    const { name, birthday, salary } = req.body;
-    if(!name || !birthday || !salary){
-        return res.status(400).send({error: 'Missing one or more required fields'})
-    }
-    const employee = await createEmployee ({ name, birthday, salary })
-    res.status(201).send(employee)
-})
-
+    
+    router.route("/").get(async ( req,res ) => {
+        const employees = await getEmployees();
+        res.send(employees);
+    });
+    
+    router.route("/").post(async ( req, res ) => {
+        if(!req.body){
+            return res.status(400).send({error: 'Missing req.body'})
+        }
+        const { name, birthday, salary } = req.body;
+        if(!name || !birthday || !salary){
+            return res.status(400).send({error: 'Missing one or more required fields'})
+        }
+        const employee = await createEmployee ({ name, birthday, salary })
+        res.status(201).send(employee)
+    })
+    
+    
+    export default router;
--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+import employeeRouter from './api/employees.js'
 import express from "express";
 const app = express();
-export default app;
 
-// TODO: this file!
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(req.method, req.originalUrl);
+    next();
+})
+app.use("/employees", employeeRouter);
+
+app.route('/').get(async ( req, res ) => {
+        res.send('Welcome to the Fullstack Employees API.')
+    })
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send('AN ERROR OCCURED' + err);
+})
+
+export default app;
